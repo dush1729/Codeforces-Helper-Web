8befebe7c394a8fab5a1d13a16942ccd8f2829ac
dush1729 <dush1729@gmail.com>
2020-02-29 22:35:30 +0530

    fetch problems

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const request = require('request')
 const app = express()
 
+const BASE_URL = "https://codeforces.com/api/"
+
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/"))
 
@@ -20,11 +22,14 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, fu
   console.log("successfully connected to database at " + new Date())
   var db = database.db("codeforceshelper")
   var contestcollection = db.collection("contest")
+  var problemcollection = db.collection("problem")
 
   fetchContests(contestcollection)
+  fetchProblems(problemcollection)
   new CronJob('0 */6 * * *', function () {
     fetchContests(contestcollection)
-  }, null, true);
+    fetchProblems(contestcollection)
+  }, null, true)
 
   app.get('/', function (req, res) {
     res.redirect("contests")
@@ -49,7 +54,7 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, fu
 })
 
 function fetchContests(contestcollection) {
-  request('https://codeforces.com/api/contest.list', function (api_error, response, body) {
+  request(BASE_URL + 'contest.list', function (api_error, response, body) {
     if (api_error) {
       throw api_error
     }
@@ -61,6 +66,25 @@ function fetchContests(contestcollection) {
           throw db_insert_err
         } else {
           console.log("contests saved successfully at " + new Date())
+        }
+      })
+    })
+  })
+}
+
+function fetchProblems(problemcollection) {
+  request(BASE_URL + 'problemset.problems', function (api_error, response, body) {
+    if (api_error) {
+      throw api_error
+    }
+
+    problems = JSON.parse(body).result.problems
+    problemcollection.deleteMany({}).then(function () {
+      problemcollection.insertMany(problems, function (db_insert_err, result) {
+        if (db_insert_err) {
+          throw db_insert_err
+        } else {
+          console.log("problems saved successfully at " + new Date())
         }
       })
     })
